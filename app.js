86a4b4f202048783498765228b84360f7149d248
Yeoh Shaun Whye <75309484+Itzshaunyeo@users.noreply.github.com>
2025-11-27 02:18:34 +0800

    Log app load and update task list on DOMContentLoaded
    Fix typo in getElementById method.

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+console.log("Campus Tasks app loaded (stub)");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const taskList = document.getElmentById("taskList");
+  taskList.textContent = "This will cause an error!";
+});
